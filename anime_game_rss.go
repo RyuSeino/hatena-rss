93d9fb5ff7fa616a209d488b075188a3ab31409a
Ryu Seino <huntinghighandlow.rs@gmail.com>
2018-01-30 12:35:42 +0900

    first commit

---
+++ anime_game_rss.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mmcdole/gofeed"
+)
+
+func main() {
+	fp := gofeed.NewParser()
+
+	feed, _ := fp.ParseURL("http://b.hatena.ne.jp/hotentry/game.rss")
+	items := feed.Items
+
+	for _, item := range items {
+		fmt.Println(item.Title)
+		fmt.Println(item.Link)
+		fmt.Println()
+	}
+
+
+}
+
